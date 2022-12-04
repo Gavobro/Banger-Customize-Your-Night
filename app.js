e9020925d21f770b1a14e979dd9b9f623369f77c
tmalenga0@gmail.com <tmalenga0@gmail.com>
2022-12-04 17:55:26 -0400

    Update JS for async functions & function to get & display movies

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-// let result = document.getElementById("result")
 var searchBtn = document.getElementById("search-btn");
 var url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 var imageElement = document.getElementById("i")
 var ingList = document.getElementById("ingred")
 // var videoElement = document.getElementById("video")
 //Movie Section
-var movieSearchBtn = document.getElementById("movieSearch-btn")
-var movieImageEl = document.getElementById("movieImage")
+const movieInp = document.getElementById("movie-inp")
+const movieImageEl = document.getElementById("movieImage")
 const apiKey="api_key=5632a8204cc4c02a4d9c1725e49763e3";
-var movieUrl="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&" +apiKey;
-var imageUrl = "https://image.tmdb.org/t/p/w500"
+const movieUrl="https://api.themoviedb.org/3";
+let imageUrl = "https://image.tmdb.org/t/p/w500"
 
-searchBtn.addEventListener("click", () => {
+searchBtn.addEventListener("click", async() => {
     let userInp = document.getElementById("user-inp").value; //******************Hopefully to add some way of detecting an error*********************
     if (userInp.length == 0){
         result.innerHTML = '<h3>Input field cannot be empty</h3>';
         console.log(userInp)
     }else{
-     fetch(url + userInp)
-       .then((response) => response.json())
-       .then((data) => {
+        let response = await fetch(url + userInp);
+        data = await response.json(); 
         let myMeal = data.meals[0];
         console.log(myMeal)
         console.log(myMeal.strMealThumb)    
@@ -60,25 +58,63 @@ searchBtn.addEventListener("click", () => {
 
         // // code to display video element
         // imageElement.setAttribute("src", myMeal.strYoutube)
-       })
-     fetch(movieUrl).then(res => res.json()).then(data => {
-        console.log(data)        
-        //for(var i==0; i<data ) -- ***************can we do a loop to loop through the array and select random movies***********
-        var myMovie = data.results[0];
-        console.log(myMovie.title)            
-                         
-        //***Comments to be added but this is to basically get the movie data and display on the page */
-        document.getElementById("movieTitle").textContent = myMovie.title
-        document.getElementById("movieOverview").textContent = myMovie.overview
-        imageUrl += myMovie.poster_path
-        movieImageEl.setAttribute("src", imageUrl)    
-
-                       
-    })
-   }
+       
+        let myMovies = await getMovies();
+        // Shuffle array
+        const shuffled = myMovies.sort(() => 0.5 - Math.random());
+        const selected = shuffled.slice(0, 6)
+        selected.forEach(showMovie)
+        //  displayMovies(myMovies);
+        }
 })
 
-// 1. Pull data with one click fix async
-// 2. Pull more than one movie
-// 3. Search types: Popularity, Kids, date in theatre
+async function getMovies() {
+    const movie_catergory ={"popularity": "/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&",
+                            "comedy":"/discover/movie?with_genres=35&sort_by=vote_average.desc&vote_count.gte=10&", 
+                            "drama": "/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=10&"}
+
+    let userChoice = document.getElementById("movie-inp").value
+
+    let response = await fetch(movieUrl+movie_catergory[userChoice]+apiKey);
+    let data = await response.json();
+        console.log(data);
+        var myMovies = data.results;
+        console.log(myMovies[0].title);
+        return myMovies;
     
+}
+
+// function displayMovies(myMovies) {
+//     document.getElementById("movieTitle").textContent = myMovie[0].title;
+//     document.getElementById("movieOverview").textContent = myMovie[0].overview;
+//     imageUrl += myMovie[0].poster_path;
+//     movieImageEl.setAttribute("src", imageUrl);
+// }
+
+
+    
+
+    
+    function showMovie(myMovie){
+        const el = document.createElement("div")
+        const movieTitle = document.createElement("h4");
+        const movieImage = document.createElement("img");
+        const movieOverview = document.createElement("h4");
+
+        movieTitle.innerHTML = myMovie.title;        
+        movieImage.setAttribute("src", imageUrl + myMovie.poster_path);
+        movieOverview.innerHTML = myMovie.overview;
+
+        el.appendChild (movieTitle);
+        el.appendChild (movieImage);
+        el.appendChild (movieOverview);
+
+        document.getElementById("movieSection").appendChild (el);
+             
+        }
+
+
+
+// 1. Pull data with one click fix async
+// 2. Pull more than one movie get error handling
+// 3. Search types: Popularity, Kids, date in theatre
